f84b16d4098f3b04d7b204285f0b2cc1f77d8798
David Fisher Evans <contact@fisherevans.com>
2015-05-10 00:14:33 -0400

    adding in daily programming

---
+++ JavaStuff/src/com/fisherevans/project_euler/Driver.java
@@ -0,0 +1,19 @@
+package com.fisherevans.com.projectEuler;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: immortal
+ * Date: 12/11/13
+ * Time: 5:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Driver
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Problem 1: " + SimpleProblems.problemOne(1000L));
+        System.out.println("Problem 2: " + SimpleProblems.problemTwo(4000000L));
+        System.out.println("Problem 3: " + SimpleProblems.problemThree(600851475143L));
+        System.out.println("Problem 4: " + SimpleProblems.problemFour(3));
+    }
+}
